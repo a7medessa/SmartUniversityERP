55be147c4d610d3ede63bc76b91064fc4ad779b4
Ahmed Eissa <a7medessa@gmail.com>
2026-07-26 20:06:01 +0300

    Add audit fields to base entity

--- src/SmartUniversityERP.Domain/Entities/BaseEntity.cs
+++ src/SmartUniversityERP.Domain/Entities/BaseEntity.cs
@@ -9,4 +9,12 @@ namespace SmartUniversityERP.Domain.Entities;
 public abstract class BaseEntity
 {
     public int Id { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public string CreatedBy { get; set; } = string.Empty;
+
+    public DateTime? LastModifiedDate { get; set; }
+
+    public string? LastModifiedBy { get; set;}
 }
